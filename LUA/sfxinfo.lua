2f838056285fc4d49f04edf5982b57242ee74bfc
El3ctr0h3ll <128558511+El3ctr0h3ll@users.noreply.github.com>
2025-12-30 21:27:52 +1100

    Add files via upload

---
+++ LUA/sfxinfo.lua
@@ -0,0 +1,17 @@
+freeslot("SFX_SPM_LINE_TURN_one", "SFX_SPM_SYSMENU_CANCEL_one")
+
+sfxinfo[SFX_SPMLINETURNone] = {
+        singular = false,
+        priority = 64,
+		flags = SF_X4AWAYSOUND,
+}
+
+sfxinfo[SFX_SPMLINETURNone].caption = "Line Turn"
+
+sfxinfo[SFX_SPMSYSMENUCANCELone] = {
+        singular = false,
+        priority = 64,
+	flags = SF_X4AWAYSOUND,
+}
+
+sfxinfo[SFX_SPMSYSMENUCANCELone].caption = "Menu cancel"
